9e61955226250236c1e47f9f657c7c0f55cbb194
LCTeixeira <52007254883@info.local>
2019-09-20 17:18:30 -0300

    2up-20/09/2019

---
+++ tabuadaDoUmAoDez/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tabuadaDoUmAoDez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x=0;x<=10;x++){
+            Console.WriteLine("Tabuadas do um ao dez");
+            Console.WriteLine($"{x} x 1= " + (1*x));
+            Console.WriteLine($"{x} x 2= " + (2*x));
+            Console.WriteLine($"{x} x 3= " + (3*x));
+            Console.WriteLine($"{x} x 4= " + (4*x));
+            Console.WriteLine($"{x} x 5= " + (5*x));
+            Console.WriteLine($"{x} x 6= " + (6*x));
+            Console.WriteLine($"{x} x 7= " + (7*x));
+            Console.WriteLine($"{x} x 8= " + (8*x));
+            Console.WriteLine($"{x} x 9= " + (9*x));
+            Console.WriteLine($"{x} x 10= " + (10*x));
+            }
+
+        }
+    }
+}
